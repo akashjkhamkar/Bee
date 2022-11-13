2e96f4fd80072c026e67a33eee7ae10bd5e3598c
Akash <akashk@onefin.in>
2022-11-13 19:39:21 +0530

    skeleton code for push
    need to tag the image
    then push

---
+++ cmd/push-cmd.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"bee/src/push"
+
+	"github.com/spf13/cobra"
+)
+
+var pushCmd = &cobra.Command{
+    Use:   "push <function-name> <hub-user>/<repo-name>[:<tag>]",
+    Short:  "Pushes the image to the repository.",
+    Args:  cobra.ExactArgs(2),
+    Run: func(cmd *cobra.Command, args []string) {
+        push.Push(args[0], args[1])
+    },
+}
+
+func init() {
+    rootCmd.AddCommand(pushCmd)
+}
---
+++ src/push/push.go
@@ -0,0 +1,6 @@
+package push
+
+func Push(function_name, repository string) {
+	// Tag the image
+	// Push the image
+}
