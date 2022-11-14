7edc9b7c8819fba71c180f56d05009899b19ef4c
Akash <akashk@onefin.in>
2022-11-14 15:38:57 +0530

    moved deployments generation in the push phase now we can edit the deployment and service file and quickly re-deploy again

--- src/push/push.go
+++ src/push/push.go
@@ -41,6 +41,64 @@ func push(repository string) {
 	}
 }
 
+func create_service_file(function_name, path string) {
+	service_config := utils.Yaml_service_config{
+		APIVersion: "v1",
+		Kind: "Service",
+	}
+
+	service_config.Metadata.Name = "bee-" + function_name + "-service"
+	service_config.Spec.Selector.BeeFunction = function_name
+	port_config := utils.Service_port{
+		Protocol: "TCP",
+		Port: 80,
+		TargetPort: 8000,
+	}
+	service_config.Spec.Ports[0] = port_config
+
+	utils.Create_yaml_config_file(service_config, path + "/Service.yaml")
+}
+
+func create_deployment_file(function_name, repository, path string, autoscaling bool, replicas int) {
+	// Deployment file
+	deployment_config := utils.Yaml_deployment{}
+
+	// apu
+	deployment_config.APIVersion = "apps/v1"
+	deployment_config.Kind = "Deployment"
+
+	// metadata
+	metadata := deployment_config.Metadata
+	metadata.Name = "bee-" + function_name
+	metadata.Labels.BeeFunction = function_name
+	deployment_config.Metadata = metadata
+
+	// spec
+	spec := deployment_config.Spec
+	spec.Replicas = replicas
+	spec.Selector.MatchLabels.BeeFunction = function_name
+	spec.Template.Metadata.Labels.BeeFunction = function_name
+
+	container_config := utils.Container_config{}
+	container_config.Name = function_name
+	container_config.Image = repository
+
+	port_config := utils.Ports_config{}
+	port_config.ContainerPort = 8000
+
+	var ports [1] utils.Ports_config
+	ports[0] = port_config
+	container_config.Ports = ports
+
+	var containers [1] utils.Container_config
+	containers[0] = container_config
+
+	spec.Template.Spec.Containers = containers
+	deployment_config.Spec = spec
+
+	utils.Create_yaml_config_file(deployment_config, path + "/Deployment.yaml")
+}
+
 func Push(path, repository string) {
 	config_file := path + "/config.yaml"
 	configs := utils.Read_yaml_config_file(config_file)
@@ -52,8 +110,13 @@ func Push(path, repository string) {
 
 	configs.Repository = repository
 
+	// tag and push
 	tag(configs.Function_name, repository)
 	push(repository)
 
+	// make deployment.yaml and service.yaml
+	// used in the deploy stage
 	utils.Create_yaml_config_file(configs, config_file)
+	create_deployment_file(configs.Function_name, repository, path, configs.Autoscaling, configs.Replicas)
+	create_service_file(configs.Function_name, path)
 }
